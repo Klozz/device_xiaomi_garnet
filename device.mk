a9ca5485476d247e7e95e1432f96d949f3eedc8a
chuspita <kosecki99@gmail.com>
2024-06-11 13:38:39 +0200

    garnet: Fix link to vibrator service

--- device.mk
+++ device.mk
@@ -541,7 +541,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    hardware/xiaomi/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
